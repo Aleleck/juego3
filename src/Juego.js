f7189644b8014015d590f42f645688d98834bec9
Alexis <panchaponal18@gmail.com>
2023-11-07 16:22:22 -0500

    cambiostarde

--- src/Juego.js
+++ src/Juego.js
@@ -12,34 +12,41 @@ class Juego extends Phaser.Scene {
         this.load.image('primera', 'inicioj.png');
         this.load.image('termo', 'termo.webp');
         this.load.image('fondo', 'fondo.png');
+        this.load.image('fond', 'fond.webp');
         this.load.image('boton', 'boton.png');
-        this.load.image('solido', 'solido.png');
+        this.load.image('deco', 'deco.webp');
+        this.load.image('lupa', 'lupa.png');
+        this.load.image('robot', 'robot.webp');
         this.load.image('particle_yellow', 'particula.png');
     }
 
     create() {
         this.add.image(0, 0, 'fondo').setOrigin(0, 0).setScale(0.6);
         this.add.image(450, 450, 'primera').setScale(0.3);
-        this.add.image(770, 500, 'termo').setScale(0.3);
-        this.add.image(300, 300, 'solido').setScale(1);
-        this.physics.world.setBounds(500, 150, 100, 100);
+        this.add.image(750, 500, 'termo').setScale(0.3).setDepth(2);
+        this.add.image(100, 500, 'deco').setScale(1);
+        this.add.image(400, 300, 'fond').setScale(1.45, 1.75).setDepth(2);
+        const robot = this.add.image(200, 320, 'robot').setScale(0.5);
+        const lupa = this.add.image(500, 300, 'lupa').setScale(0.7); // Crea la capa y define su posición
+        this.physics.world.setBounds(520, 200, 90, 90);
 
         const barraRoja = this.add.graphics();
+        
         const vid1 = this.add.video(450, 450, 'vid1').setScale(0.3);
         const vid2 = this.add.video(450, 450, 'vid2').setScale(0.3);
         const vid3 = this.add.video(450, 450, 'vid3').setScale(0.3);
         const vid4 = this.add.video(450, 450, 'vid4').setScale(0.3);
 
-        barraRoja.fillStyle(0xff0000, 1).fillRect(765, 555, 10, 10);
-        const playButton = this.add.image(770, 400, 'boton');
+        barraRoja.fillStyle(0xff0000, 1).fillRect(745, 555, 10, 10).setDepth(2);
+        const playButton = this.add.image(750, 390, 'boton').setDepth(2);
         playButton.setInteractive();
 
         const particles = this.physics.add.group({
             key: 'particle_yellow',
-            frameQuantity: 500,
+            frameQuantity: 300,
             collideWorldBounds: true,
-            bounceX: 0.8,
-            bounceY: 0.8,
+            bounceX: 0.91,
+            bounceY: 0.91,
         });
 
         this.physics.add.collider(particles, particles);
@@ -49,16 +56,10 @@ class Juego extends Phaser.Scene {
                 Phaser.Math.Between(300, 500),
                 Phaser.Math.Between(200, 400)
             );
-            particle.setScale(0.3);
+            particle.setScale(0.2);
             particle.body.allowGravity = false;
-            particle.setVelocity(Phaser.Math.Between(-10, 1), Phaser.Math.Between(-10, 1)); // Valores más bajos
+            particle.setVelocity(Phaser.Math.Between(-20, 20), Phaser.Math.Between(-20, 20)); // Valores más bajos
         });
-
-        const actualizarVelocidadParticulas = () => {
-            const velocidadParticulas = Phaser.Math.Linear(50, 300, longitudBarra / longitudMaxima);
-            particles.setVelocityX(velocidadParticulas);
-            particles.setVelocityY(velocidadParticulas);
-        };
 
         let numClicks = 0; // Contador de clics
 
@@ -71,19 +72,23 @@ class Juego extends Phaser.Scene {
 
                 // Limpia la barra antes de redibujar
                 barraRoja.clear();
-                barraRoja.fillStyle(0xff0000, 1).fillRect(765, 555 - barHeight, 10, barHeight);
+                barraRoja.fillStyle(0xff0000, 1).fillRect(745, 555 - barHeight, 10, barHeight);
 
                 // Inicia el video correspondiente basado en el número de clics
                 if (numClicks === 1) {
-                    vid1.play(true);
+                    vid1.play(true).setDepth(1);
+                    lupa.setDepth(2);
+                    robot.setDepth(2);
                     for (let i = 0; i < 200; i++) {
                         const particle = particles.getChildren()[i];
                         if (particle) {
                             particle.destroy();
                         }
                     }
-                } else if (numClicks === 2) {
+                }  else if (numClicks === 2) {
+                    vid1.setVisible(false);
                     vid2.play(true);
+                    //lupa.setDepth(2);
                     for (let i = 0; i < 200; i++) {
                         const particle = particles.getChildren()[i];
                         if (particle) {
@@ -92,16 +97,19 @@ class Juego extends Phaser.Scene {
                     }
                     
                 } else if (numClicks === 3) {
+                    vid2.setVisible(false);
                     vid3.play(true);
-                    for (let i = 0; i < 200; i++) {
+                    
+                    for (let i = 0; i < 50; i++) {
                         const particle = particles.getChildren()[i];
                         if (particle) {
                             particle.destroy();
                         }
                     }                 
                 } else if (numClicks === 4) {
+                    vid3.setVisible(false);
                     vid4.play(true);
-                    for (let i = 0; i < 200; i++) {
+                    for (let i = 0; i < 40; i++) {
                         const particle = particles.getChildren()[i];
                         if (particle) {
                             particle.destroy();

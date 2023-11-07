7c7436055f8b072f528ebfc8629a768a89bcf30b
Alexis <panchaponal18@gmail.com>
2023-11-07 13:48:26 -0500

    cambios

--- src/Juego.js
+++ src/Juego.js
@@ -14,6 +14,7 @@ class Juego extends Phaser.Scene {
         this.load.image('fondo', 'fondo.png');
         this.load.image('boton', 'boton.png');
         this.load.image('solido', 'solido.png');
+        this.load.image('particle_yellow', 'particula.png');
     }
 
     create() {
@@ -21,7 +22,8 @@ class Juego extends Phaser.Scene {
         this.add.image(450, 450, 'primera').setScale(0.3);
         this.add.image(770, 500, 'termo').setScale(0.3);
         this.add.image(300, 300, 'solido').setScale(1);
-        
+        this.physics.world.setBounds(500, 150, 100, 100);
+
         const barraRoja = this.add.graphics();
         const vid1 = this.add.video(450, 450, 'vid1').setScale(0.3);
         const vid2 = this.add.video(450, 450, 'vid2').setScale(0.3);
@@ -31,6 +33,32 @@ class Juego extends Phaser.Scene {
         barraRoja.fillStyle(0xff0000, 1).fillRect(765, 555, 10, 10);
         const playButton = this.add.image(770, 400, 'boton');
         playButton.setInteractive();
+
+        const particles = this.physics.add.group({
+            key: 'particle_yellow',
+            frameQuantity: 500,
+            collideWorldBounds: true,
+            bounceX: 0.8,
+            bounceY: 0.8,
+        });
+
+        this.physics.add.collider(particles, particles);
+
+        particles.children.iterate((particle) => {
+            particle.setPosition(
+                Phaser.Math.Between(300, 500),
+                Phaser.Math.Between(200, 400)
+            );
+            particle.setScale(0.3);
+            particle.body.allowGravity = false;
+            particle.setVelocity(Phaser.Math.Between(-10, 1), Phaser.Math.Between(-10, 1)); // Valores más bajos
+        });
+
+        const actualizarVelocidadParticulas = () => {
+            const velocidadParticulas = Phaser.Math.Linear(50, 300, longitudBarra / longitudMaxima);
+            particles.setVelocityX(velocidadParticulas);
+            particles.setVelocityY(velocidadParticulas);
+        };
 
         let numClicks = 0; // Contador de clics
 
@@ -48,16 +76,45 @@ class Juego extends Phaser.Scene {
                 // Inicia el video correspondiente basado en el número de clics
                 if (numClicks === 1) {
                     vid1.play(true);
+                    for (let i = 0; i < 200; i++) {
+                        const particle = particles.getChildren()[i];
+                        if (particle) {
+                            particle.destroy();
+                        }
+                    }
                 } else if (numClicks === 2) {
                     vid2.play(true);
+                    for (let i = 0; i < 200; i++) {
+                        const particle = particles.getChildren()[i];
+                        if (particle) {
+                            particle.destroy();
+                        }
+                    }
+                    
                 } else if (numClicks === 3) {
                     vid3.play(true);
+                    for (let i = 0; i < 200; i++) {
+                        const particle = particles.getChildren()[i];
+                        if (particle) {
+                            particle.destroy();
+                        }
+                    }                 
                 } else if (numClicks === 4) {
                     vid4.play(true);
+                    for (let i = 0; i < 200; i++) {
+                        const particle = particles.getChildren()[i];
+                        if (particle) {
+                            particle.destroy();
+                        }
+                    }
                 }
             }
         });
-        
+
+        this.physics.world.setBoundsCollision(true, true, true, true);
+
+
+
     }
 
     update(time, delta) {
